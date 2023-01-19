6609418ee8a7a709904cb7497057c79418c85b96
keenset1 <laurent14511@gmail.com>
2023-01-19 15:08:52 -0500

    hey'

---
+++ routes/favorite.py
@@ -0,0 +1,7 @@
+from flask import render_template
+from app import app
+
+@app.route("/favorite-5")
+def favorite_5():
+    favorite_5 = ["sports1", "sports2", "sports3", "sports4", "sports5"]
+    return render_template("favorite_5.html", favorite_5=favorite_5)
